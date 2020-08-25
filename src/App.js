6b08db786cff24a7d57d8ad2a00eb173f0519bd3
hovanco <hovancoquangtri@gmail.com>
2020-08-25 11:11:04 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,182 @@
+import React, { Component } from 'react';
+import './App.css';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <div className="navmenu">
+          <ul>
+            <li>
+              <a href="#">Link Item 1</a>
+              <ul className="megamenu">
+                <li>
+                  <h1>Title</h1>
+                  <a href="#">Submenu 1</a>
+                  <a href="#">Submenu 2</a>
+                  <a href="#">Submenu 3</a>
+                  <a href="#">Submenu 4</a>
+                  <a href="#">Submenu 5</a>
+                  <a href="#">Submenu 6</a>
+                  <a href="#">Submenu 7</a>
+                </li>
+                <li>
+                  <h1>Title</h1>
+                  <a href="#">Submenu 1</a>
+                  <a href="#">Submenu 2</a>
+                  <a href="#">Submenu 3</a>
+                  <a href="#">Submenu 4</a>
+                  <a href="#">Submenu 5</a>
+                  <a href="#">Submenu 6</a>
+                  <a href="#">Submenu 7</a>
+                </li>
+                <li>
+                  <h1>Title</h1>
+                  <a href="#">Submenu 1</a>
+                  <a href="#">Submenu 2</a>
+                  <a href="#">Submenu 3</a>
+                  <a href="#">Submenu 4</a>
+                  <a href="#">Submenu 5</a>
+                  <a href="#">Submenu 6</a>
+                  <a href="#">Submenu 7</a>
+                </li>
+                <li>
+                  <h1>Title</h1>
+                  <a href="#">Submenu 1</a>
+                  <a href="#">Submenu 2</a>
+                  <a href="#">Submenu 3</a>
+                  <a href="#">Submenu 4</a>
+                  <a href="#">Submenu 5</a>
+                  <a href="#">Submenu 6</a>
+                  <a href="#">Submenu 7</a>
+                </li>
+              </ul>
+            </li>
+            <li><a href="#">Link Item 2</a>
+              <ul className="megamenu">
+                <li>
+                  <h1>Title</h1>
+                  <a href="#">Submenu 1</a>
+                  <a href="#">Submenu 2</a>
+                  <a href="#">Submenu 3</a>
+                  <a href="#">Submenu 4</a>
+                  <a href="#">Submenu 5</a>
+                  <a href="#">Submenu 6</a>
+                  <a href="#">Submenu 7</a>
+                </li>
+                <li>
+                  <h1>Title</h1>
+                  <a href="#">Submenu 1</a>
+                  <a href="#">Submenu 2</a>
+                  <a href="#">Submenu 3</a>
+                  <a href="#">Submenu 4</a>
+                  <a href="#">Submenu 5</a>
+                  <a href="#">Submenu 6</a>
+                  <a href="#">Submenu 7</a>
+                </li>
+                <li>
+                  <h1>Title</h1>
+                  <a href="#">Submenu 1</a>
+                  <a href="#">Submenu 2</a>
+                  <a href="#">Submenu 3</a>
+                  <a href="#">Submenu 4</a>
+                  <a href="#">Submenu 5</a>
+                  <a href="#">Submenu 6</a>
+                  <a href="#">Submenu 7</a>
+                </li>
+                <li>
+                  <h1>Title</h1>
+                  <a href="#">Submenu 1</a>
+                  <a href="#">Submenu 2</a>
+                  <a href="#">Submenu 3</a>
+                  <a href="#">Submenu 4</a>
+                  <a href="#">Submenu 5</a>
+                  <a href="#">Submenu 6</a>
+                  <a href="#">Submenu 7</a>
+                </li>
+              </ul>
+            </li>
+            <li><a href="#">Link Item 3</a>
+              <ul className="megamenu">
+                <li>
+                  <div className="megamenudiv">
+                    <h1>Title</h1>
+                    <p>
+                      Cristiano Ronaldo dos Santos Aveiro GOIH ComM (European Portuguese: [kɾiʃˈtjɐnu ʁɔˈnaɫdu]; born 5 February 1985) is a Portuguese professional footballer who plays as a forward for Serie A club Juventus and captains the Portugal national team. Often considered the best player in the world and widely regarded as one of the greatest players of all time.
+                    </p>
+                  </div>
+                </li>
+                <li>
+                  <div className="megamenudiv">
+                    <h1>Title</h1>
+                    <p>
+                      Cristiano Ronaldo dos Santos Aveiro GOIH ComM (European Portuguese: [kɾiʃˈtjɐnu ʁɔˈnaɫdu]; born 5 February 1985) is a Portuguese professional footballer who plays as a forward for Serie A club Juventus and captains the Portugal national team. Often considered the best player in the world and widely regarded as one of the greatest players of all time.
+                    </p>
+                  </div>
+                </li>
+                <li>
+                  <h1>Title</h1>
+                  <a href="#">Submenu 1</a>
+                  <a href="#">Submenu 2</a>
+                  <a href="#">Submenu 3</a>
+                  <a href="#">Submenu 4</a>
+                  <a href="#">Submenu 5</a>
+                  <a href="#">Submenu 6</a>
+                  <a href="#">Submenu 7</a>
+                </li>
+              </ul>
+            </li>
+            <li><a href="#">Link Item 4</a>
+              <ul className="megamenu">
+                <li>
+                  <h1>Title</h1>
+                  <a href="#">Submenu 1</a>
+                  <a href="#">Submenu 2</a>
+                  <a href="#">Submenu 3</a>
+                  <a href="#">Submenu 4</a>
+                  <a href="#">Submenu 5</a>
+                  <a href="#">Submenu 6</a>
+                  <a href="#">Submenu 7</a>
+                </li>
+                <li>
+                  <h1>Title</h1>
+                  <a href="#">Submenu 1</a>
+                  <a href="#">Submenu 2</a>
+                  <a href="#">Submenu 3</a>
+                  <a href="#">Submenu 4</a>
+                  <a href="#">Submenu 5</a>
+                  <a href="#">Submenu 6</a>
+                  <a href="#">Submenu 7</a>
+                </li>
+              </ul>
+            </li>
+            <li><a href="#">Link Item 5</a>
+              <ul className="megamenu">
+                <li>
+                  <h1>Title</h1>
+                  <a href="#">Submenu 1</a>
+                  <a href="#">Submenu 2</a>
+                  <a href="#">Submenu 3</a>
+                  <a href="#">Submenu 4</a>
+                  <a href="#">Submenu 5</a>
+                  <a href="#">Submenu 6</a>
+                  <a href="#">Submenu 7</a>
+                </li>
+                <li>
+                  <h1>Title</h1>
+                  <a href="#">Submenu 1</a>
+                  <a href="#">Submenu 2</a>
+                  <a href="#">Submenu 3</a>
+                  <a href="#">Submenu 4</a>
+                  <a href="#">Submenu 5</a>
+                  <a href="#">Submenu 6</a>
+                  <a href="#">Submenu 7</a>
+                </li>
+              </ul>
+            </li>
+          </ul>
+        </div>
+      </div>
+    )
+  }
+}
